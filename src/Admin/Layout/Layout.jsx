553aa209d18b9f868d81afad7ee76c4f1c53a538
DaikonCOde <daikon.code@gmail.com>
2022-01-24 22:09:25 -0500

    layout

--- src/Admin/Layout/Layout.jsx
+++ src/Admin/Layout/Layout.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import { Outlet } from "react-router-dom";
 
 import { useSelector } from "react-redux";
-import { Navigate } from "react-router-dom";
 import NavAdminMenu from "../Components/NavAdminMenu/NavAdminMenu";
 
 import { ContentDashboardLayout } from "./LayoutStyles";
